92552578e190c115e580a93376378f83ad158c92
Thanat <bztzn512@gmail.com>
2021-09-11 16:01:37 -0400

    addPicures

--- reCreateWebsite/header.js
+++ reCreateWebsite/header.js
@@ -1,7 +1,33 @@
 const menuBtn = document.getElementById("menu-btn");
+const colorBtn = document.getElementById("color-btn");
+const themeBtn = document.getElementById("theme-btn");
 
 const menuBar = document.querySelector(".menu-bar");
+const colorsPalette = document.querySelector(".colors-palette");
+const btnColorChange = document.querySelectorAll(".colors-palette .color");
 
 menuBtn.onclick = () => {
   menuBar.classList.toggle("active");
+  colorsPalette.classList.remove("active");
 };
+
+colorBtn.onclick = () => {
+  colorsPalette.classList.toggle("active");
+  menuBar.classList.remove("active");
+};
+
+themeBtn.onclick = () => {
+  themeBtn.classList.toggle("fa-sun");
+  if (themeBtn.classList.contains("fa-sun")) {
+    document.body.classList.add("active");
+  } else {
+    document.body.classList.remove("active");
+  }
+};
+
+btnColorChange.forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--main-color", color);
+  };
+});
